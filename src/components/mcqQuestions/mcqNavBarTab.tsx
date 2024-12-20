d20882e14484273b4c7c6bdbb65ff001c919f021
Sicelo_Mduna <sicelo.mduna@globalmicro.co.za>
2024-12-20 11:43:06 +0200

    mcqBody.tsx

--- src/components/mcqQuestions/mcqNavBarTab.tsx
+++ src/components/mcqQuestions/mcqNavBarTab.tsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import McqToolbar from "./mcqToolbar";
 import McqBody from "./mcqBody";
+import McqBodyScoreCalculator from "./mcqBodyScoreCalculator";
+import McqToolBarQuestionNavigator from "./mcqToolBarQuestionNavigator";
+import { questions } from "./mcqBodyQuestions";
 
 interface McqNavBarTabProps {
   handleNavigationChange: (navTarget: string, routeParams?: {}) => void;
 }
 
-interface McqNavBarTabState {}
 
-class McqNavBarTab extends React.Component<
-  McqNavBarTabProps,
-  McqNavBarTabState
-> {
-  state = { messages: null };
-  render() {
+const McqNavBarTab: React.FunctionComponent<McqNavBarTabProps> = ({ handleNavigationChange }) => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const [selectedAnswers, setSelectedAnswers] = useState<number[]>(
+    new Array(questions.length).fill(null));
+  const [showScore, setShowScore] = useState(false);
+
+  const passPercentage = 60;
+
+  const getQuizScore = (): number => {
+    return selectedAnswers.reduce((acc, answer, index) => {
+      if (answer === questions[index].correctAnswer) return acc + 1;
+      return acc;
+    }, 0);
+  };
+
+  const checkIfPassed = (): boolean => {
+    const score = getQuizScore();
+    return (score / questions.length) * 100 >= passPercentage;
+  };
+
+  const resetQuiz = () => {
+    setCurrentQuestionIndex(0);
+    setSelectedAnswers(new Array(questions.length).fill(null));
+    setShowScore(false);
+  };
+
+
+  const restartQuiz = () => {
+    resetQuiz();
+  };
+
+  const handleBlockClick = (index: number) => {
+    setCurrentQuestionIndex(index);
+  };
+
     return (
       <div className="invoices-due-container">
         <McqToolbar />
@@ -23,14 +55,30 @@ class McqNavBarTab extends React.Component<
           id="content-area"
         >
           <div className="container-fluid">
-            <div className="invoices-due-container">
-              <McqBody />
+            <div className="mcq-questions-container">
+              {showScore ? (
+                <McqBodyScoreCalculator
+                  calculateScore={getQuizScore}
+                  isPassed={checkIfPassed}
+                  handleRetake={restartQuiz}
+                />) 
+                : 
+                (
+                <McqBody
+                  questions={questions}
+                  currentQuestionIndex={currentQuestionIndex}
+                  setCurrentQuestionIndex={setCurrentQuestionIndex}
+                  selectedAnswers={selectedAnswers}
+                  setSelectedAnswers={setSelectedAnswers}
+                  onSubmitQuiz={() => setShowScore(true)}
+                />
+                )
+              }
             </div>
           </div>
         </div>
       </div>
     );
   }
-}
 
 export default McqNavBarTab;
